ccfcef2a5b4f7c1eb673ffcc1f1274ba569b45bf
kblagg <kblagg@CVB-579156>
2025-10-01 13:49:02 -0400

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace NACE_Match_Build
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
